af92c07401e30a27fc1842676829890cba471a22
Roberto Ross Leon <dev@rross.me>
2020-01-10 08:37:20 -0600

    Function showScore() added for two players. Needs modification to show only for one player.

--- main.py
+++ main.py
@@ -5,20 +5,31 @@ pygame.init()
 
 screen = pygame.display.set_mode((800,600))
 
-font = pygame.font.Font('freesansbold.ttf', 32)
-textX = 400
+# Color font
+color_font = pygame.font.Font('freesansbold.ttf', 128)
+# Color word position
+textX = 250
 textY = 300
 
-text = font.render('Color', True, (255,255,255))
-screen.blit(text, (textX, textY))
+# Score font
+score_font = pygame.font.Font('freesansbold.ttf', 32)
+# Player 1 score position
+scoreX = 10
+scoreY = 10
 
+player_1_score = 0
+player_2_score = 0
 
 running = True
 screen.fill((15,50,200))
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
 
 def newBattle():
     colors = ['Rojo', 'Verde', 'Azul']
-    rgb_colors = [(255,0,0), (0,255,0), (0,0,255)]
+
+    rgb_colors = [RED, GREEN, BLUE]
 
     background_color = random.choice(rgb_colors)
     font_color = random.choice(rgb_colors)
@@ -26,24 +37,37 @@ def newBattle():
         font_color = random.choice(rgb_colors)
 
     screen.fill(background_color)
-    text = font.render(random.choice(colors), True, font_color)
+    text = color_font.render(random.choice(colors), True, font_color)
     screen.blit(text, (textX, textY))
 
+    return font_color
+
+def showScore():
+    score_text = score_font.render('Player 1: ' + str(player_1_score), True, (255,255,255))
+    screen.blit(score_text, (scoreX, scoreY))
+    score_text = score_font.render('Player 2: ' + str(player_2_score), True, (255,255,255))
+    screen.blit(score_text, (scoreX + 600, scoreY))
+
+game_state = 'GAME'
+color = BLUE
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-        mensaje = ''
-
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
-                mensaje = 'Se presiono A'
-            if event.key == pygame.K_s:
-                mensaje = 'Se presiono S'
-            if event.key == pygame.K_d:
-                mensaje = 'Se presiono D'
-
-            newBattle()
+        if game_state == 'GAME':
+            if event.type == pygame.KEYUP:
+                color = newBattle()
+                showScore()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a:
+                    if color == RED:
+                        player_1_score += 1
+                if event.key == pygame.K_s:
+                    if color == GREEN:
+                        player_1_score += 1
+                if event.key == pygame.K_d:
+                    if color == BLUE:
+                        player_1_score += 1
 
         pygame.display.update()
